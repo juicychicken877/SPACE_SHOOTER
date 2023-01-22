3c15e63354847f3a9f7d8d7e12ab2e575f5c827a
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-22 22:26:23 +0100

    Update player.py

--- Space/player.py
+++ Space/player.py
@@ -1,17 +1,26 @@
 from settings import *
-from declarations import player_image, shoot
-from bullets import Bullet
+from bullets import PlayerBullet
 
 
 class Player:
     def __init__(self):
-        self.image = player_image
-        self.rect = self.image.get_rect(midbottom=(300, 700))
+        self.image = pygame.image.load('images/xcf/ship-1.xcf').convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(300, 680))
+        self.speed = 7
+        # self.HP = 5
+        self.ammo = 100
+        self.max_ammo = 100
 
-        self.speed = 5
+        self.shoot_cooldown = 100
+        self.shots_fired = 0
 
-        self.shoot_cooldown = 150
+        self.shoot_sound = pygame.mixer.Sound('sounds/shoot.wav')
+        self.shoot_sound.set_volume(0.1)
+        # self.no_ammo_warning = font.render('NO AMMO', False, 'Red').convert()
+        # self.ammo_warning_rect = self.no_ammo_warning.get_rect(center=(300, 400))
 
+        # self.game_over_warning = font.render('GAME OVER', False, 'Red').convert()
+        # self.game_over_rect = self.game_over_warning.get_rect(center=(300,400))
         self.bullet_list = pygame.sprite.Group()
 
     def input(self):
@@ -19,14 +28,14 @@ class Player:
 
         if keys[pygame.K_a]:
             # going to -x
-            if self.rect.left != 0:
+            if self.rect.left >= 0:
                 self.rect.x = self.rect.x - self.speed
             else:
                 pass
 
         if keys[pygame.K_d]:
             # going to +x
-            if self.rect.right != 600: 
+            if self.rect.right <= 600: 
                 self.rect.x = self.rect.x + self.speed
             else:
                 pass
@@ -38,19 +47,39 @@ class Player:
             pass
 
     def shoot(self):
-        shoot.play()
+        # if self.ammo > 0:
+        self.shoot_sound.play()
+        # self.ammo -= 1
+        self.bullet_list.add(PlayerBullet(self.rect.centerx))
+        self.shoot_cooldown = 100
+        self.shots_fired += 1
 
-        self.bullet_list.add(Bullet(self.rect.centerx))
+        # elif self.ammo == 0:
+            # if dont have ammo
+            # screen.blit(self.no_ammo_warning, self.ammo_warning_rect)
+    def displayShotsFiredHud(self):
+        shots_fired_surf = font2.render('SHOTS FIRED:', False, 'Black').convert()
+        shots_fired_count = font2.render(f'{self.shots_fired}', False, 'Black').convert()
+        screen.blit(shots_fired_surf, (10, 730))
+        screen.blit(shots_fired_count, (200, 730))
 
-        self.shoot_cooldown = 150
+    def isShot(self, enemies_group):
+        for sprite in enemies_group:
+            for bullet in sprite.bullets_list:
+                # if bullet collides with a player
+                if bullet.rect.bottom >= self.rect.top and bullet.rect.left >= self.rect.left and bullet.rect.right <= self.rect.right:
+                    bullet.kill()
+                else:
+                    pass
 
-    def update(self):
+    def update(self, enemies_group):
         # decreasing the cooldown every frame
         if self.shoot_cooldown > 0:
             self.shoot_cooldown -= 10
         # bullet list
         self.bullet_list.draw(screen)
         self.bullet_list.update()
-
+        self.isShot(enemies_group)
         self.input()
+        self.displayShotsFiredHud()
         screen.blit(self.image, self.rect) # draw player on the screen
