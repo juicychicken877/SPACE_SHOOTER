a371dd0dc7929a81a71b29429b8bd87f44c1b43b
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-22 22:23:59 +0100

    Update background.py

--- Space/background.py
+++ Space/background.py
@@ -1,11 +1,10 @@
-from declarations import background_black1, background_black2
-from settings import screen
+from settings import *
 
 
 class AnimatedBackground:
     def __init__(self):
-        self.image1 = background_black1
-        self.image2 = background_black2
+        self.image1 = pygame.image.load('images/backgrounds/space-background-2-black.jpg').convert_alpha()
+        self.image2 = self.image1
 
         self.image1_rect = self.image1.get_rect(topleft=(0,0))
         self.image2_rect = self.image2.get_rect(topleft=(0,-1250))
@@ -13,7 +12,7 @@ class AnimatedBackground:
         self.y1 = 0
         self.y2 = -1250
 
-        self.speed = 3
+        self.speed = 1
 
     def move(self):
         self.image1_rect.y += self.speed
