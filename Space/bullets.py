725b54c3f1d3d2372fbefabf90cc2e333dc45710
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-22 22:25:03 +0100

    Update bullets.py

--- Space/bullets.py
+++ Space/bullets.py
@@ -1,11 +1,11 @@
 from settings import *
-from declarations import bullet
 
-class Bullet(pygame.sprite.Sprite):
+
+class PlayerBullet(pygame.sprite.Sprite):
     def __init__(self, player_x):
         super().__init__()
 
-        self.image = bullet
+        self.image = pygame.image.load('images/default/bullet-red.png').convert_alpha()
         self.rect = self.image.get_rect(center=(player_x, 650))
 
         self.speed = 8
@@ -17,7 +17,31 @@ class Bullet(pygame.sprite.Sprite):
         # if out of screen kill bullet
         if (self.rect.y <= 0):
             self.kill()
+    
+    def update(self):
+        self.move_bullet()
+        self.isOutOfScreen()
 
+        
+class SpaceInvaderBullet(pygame.sprite.Sprite):
+    def __init__(self, invader_x, invader_y):
+        super().__init__()
+
+        self.image = pygame.image.load('images/default/bullet-red.png').convert_alpha()
+        self.rect = self.image.get_rect(center=(invader_x, invader_y))
+
+        self.speed = 10
+
+    def move_bullet(self):
+        self.rect.y += self.speed
+
+    def isOutOfScreen(self):
+        # if out of screen kill bullet
+        if (self.rect.y >= 800):
+            self.kill()
+    
     def update(self):
         self.isOutOfScreen()
         self.move_bullet()
+        
+
