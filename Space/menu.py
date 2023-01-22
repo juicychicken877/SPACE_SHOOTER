4e8a45e1e46bd724e7c1e4fa4b53b48ef6d9a1e9
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-22 22:25:55 +0100

    Update menu.py

--- Space/menu.py
+++ Space/menu.py
@@ -1,11 +1,19 @@
 from settings import *
-from declarations import *
+from bars import *
 
 
 class MainMenu:
     def __init__(self):
-        self.background = background_black1
+        self.background = pygame.image.load('images/backgrounds/main-menu-background-1.jpg').convert_alpha()
+        self.start_bar = MainMenuStartGameBar()
+        self.logo = MainMenuAnimatedLogo()
+        self.exit_bar = MainMenuExitBar()
 
-    def draw(self):
+    def update(self):
         screen.blit(self.background, (0, 0))
+        self.logo.update()
+        self.exit_bar.update()
+        
+        return self.start_bar.update()
+
 
