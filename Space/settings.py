fa04c6c70120e7cd74d88468c5dd386efd0c0450
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-22 22:28:11 +0100

    Update settings.py

--- Space/settings.py
+++ Space/settings.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame, sys
+from random import randint
 
 pygame.init()
 
@@ -7,5 +8,10 @@ HEIGHT = 800
 FPS = 60
 WINDOW_TITLE = 'Space Shooter'
 
+font = pygame.font.Font('font/stormfaze.ttf', 50)
+font2 = pygame.font.Font('font/stormfaze.ttf', 20)
+pygame.display.set_caption(WINDOW_TITLE)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+
