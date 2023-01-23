372267b4dd637a4c21f73644ffde9d9454dca2e4
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-23 21:13:18 +0100

    Update player.py

--- Space/player.py
+++ Space/player.py
@@ -1,26 +1,20 @@
 from settings import *
 from bullets import PlayerBullet
 
-
 class Player:
     def __init__(self):
         self.image = pygame.image.load('images/xcf/ship-1.xcf').convert_alpha()
         self.rect = self.image.get_rect(midbottom=(300, 680))
         self.speed = 7
-        # self.HP = 5
+
         self.ammo = 100
         self.max_ammo = 100
 
-        self.shoot_cooldown = 100
-        self.shots_fired = 0
+        self.shoot_cooldown = 200
 
         self.shoot_sound = pygame.mixer.Sound('sounds/shoot.wav')
         self.shoot_sound.set_volume(0.1)
-        # self.no_ammo_warning = font.render('NO AMMO', False, 'Red').convert()
-        # self.ammo_warning_rect = self.no_ammo_warning.get_rect(center=(300, 400))
 
-        # self.game_over_warning = font.render('GAME OVER', False, 'Red').convert()
-        # self.game_over_rect = self.game_over_warning.get_rect(center=(300,400))
         self.bullet_list = pygame.sprite.Group()
 
     def input(self):
@@ -47,21 +41,9 @@ class Player:
             pass
 
     def shoot(self):
-        # if self.ammo > 0:
         self.shoot_sound.play()
-        # self.ammo -= 1
         self.bullet_list.add(PlayerBullet(self.rect.centerx))
-        self.shoot_cooldown = 100
-        self.shots_fired += 1
-
-        # elif self.ammo == 0:
-            # if dont have ammo
-            # screen.blit(self.no_ammo_warning, self.ammo_warning_rect)
-    def displayShotsFiredHud(self):
-        shots_fired_surf = font2.render('SHOTS FIRED:', False, 'Black').convert()
-        shots_fired_count = font2.render(f'{self.shots_fired}', False, 'Black').convert()
-        screen.blit(shots_fired_surf, (10, 730))
-        screen.blit(shots_fired_count, (200, 730))
+        self.shoot_cooldown = 200
 
     def isShot(self, enemies_group):
         for sprite in enemies_group:
@@ -81,5 +63,5 @@ class Player:
         self.bullet_list.update()
         self.isShot(enemies_group)
         self.input()
-        self.displayShotsFiredHud()
-        screen.blit(self.image, self.rect) # draw player on the screen
+        # draw player on the screen
+        screen.blit(self.image, self.rect)
