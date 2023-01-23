96cf72df439681babe4806cd59f840a2d6b132b3
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2023-01-23 21:12:43 +0100

    Update enemies.py

--- Space/enemies.py
+++ Space/enemies.py
@@ -3,17 +3,28 @@ from bullets import SpaceInvaderBullet
 
 
 class SpaceInvader(pygame.sprite.Sprite):
-    def __init__(self, x):
+    def __init__(self, x, type):
         super().__init__()
-        self.image = pygame.image.load('images/default/space-invader.png').convert_alpha()
+        self.image_blue = pygame.image.load('images/default/space-invader.png').convert_alpha()
+        self.image_green = pygame.image.load('images/default/space-invader-green.jpg').convert_alpha()
+        self.image_purple = pygame.image.load('images/default/space-invader-purple.png').convert_alpha()
+        
+        if type == 'blue':
+            self.image = self.image_blue
+        elif type == 'green':
+            self.image = self.image_green
+        else:
+            self.image = self.image_purple
+
         self.rect = self.image.get_rect(center=(x, -100))
 
         self.HP = 100
-        self.speed = 2
-        self.shot_cooldown = 2000
-        self.count = 0
+        self.speed = 3
+        self.shot_cooldown = 3000
+
         self.shot_sound = pygame.mixer.Sound('sounds/shoot.wav')
         self.shot_sound.set_volume(0.1)
+
         self.bullets_list = pygame.sprite.Group()
 
     def move(self):
@@ -22,26 +33,19 @@ class SpaceInvader(pygame.sprite.Sprite):
     def isCollidingWithBullet(self, bullet_list):
         # kill a bullet if colliding with enemy
         if pygame.sprite.spritecollide(self, bullet_list, True):
-            self.HP -= randint(10, 40)
+            self.kill()
 
     def attack(self):
         if self.shot_cooldown <= 0:
             self.bullets_list.add(SpaceInvaderBullet(self.rect.centerx, self.rect.centery))
-            self.shot_cooldown = randint(1500, 2500)
+            self.shot_cooldown = randint(3000, 3500)
             self.shot_sound.play()
-
-    def drawHPBar(self):
-        pygame.draw.rect(screen, 'Red', pygame.Rect(self.rect.x, self.rect.y-20, self.HP, 10))
 
     def isAlive(self):
         if self.rect.y > 900:
-            print('space invader killed by black hole')
-            print('======')
             self.kill()
-        if self.HP > 0:
-            pass
         else:
-            self.kill()
+            pass
 
     def update(self, bullet_list):
         if self.shot_cooldown > 0:
@@ -49,7 +53,6 @@ class SpaceInvader(pygame.sprite.Sprite):
         self.bullets_list.draw(screen)
         self.bullets_list.update()
         self.attack()
-        self.drawHPBar()
         self.move()
         self.isAlive()
         self.isCollidingWithBullet(bullet_list)
